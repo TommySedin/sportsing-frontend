62eee36d213ba7026327bc837199e634926da2b5
tommy <tommy.sedin@adopi.se>
2017-11-08 11:46:32 +0100

    First!

---
+++ src/main/java/com/sportsing/frontend/SportsingBean.java
@@ -0,0 +1,13 @@
+package com.sportsing.frontend;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class SportsingBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+}
